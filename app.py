0767daecea991446aed07f64fb3e79e52e8c2db3
HGGshiwo <3190102788@zju.edu.cn>
2022-05-07 16:06:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-from asyncio.windows_events import NULL
-import requests
-import web
-import json
+import requests, web, json
 
 urls = (
     '/history(.*)', 'history_index',
@@ -25,17 +22,27 @@ def to_json(obj):
 def get_url():
     return web.ctx.fullpath
 
+def to_list(obj):
+    res = []
+    for data in obj:
+        res.append(to_json(data))
+    return res
+
 class history_index:
     def POST(self, data):
         # 往数据库插入一条垃圾放置数据
         db.insert('history_tb', key=data.time, value=json.dumps(data))
+        # 更新垃圾桶当前的容量
+        id = data.position
+        res = db.select('settings_tb', what="value", where={'settings_tb.key': id})
+        res = to_json(res[0])
+        res.curCapacity += 1
+        db.update('settings_tb', where={'settings_tb.key': id}, value=json.dumps(res))
     
     def GET(self, data):
         # 返回所有垃圾放置数据
-        data_list = db.select('history_tb', what="value")
-        res = []
-        for data in data_list:
-            res.append(to_json(data))
+        res = db.select('history_tb', what="value")
+        res = to_list(res)
         web.header("Access-Control-Allow-Origin", "*")
         web.header("Content-Type", "application/json")
         return json.dumps(res)
@@ -43,10 +50,10 @@ class history_index:
 class settings_index:
     
     def GET(self, id):
-        # 查询数据库，返回数据
-        if id is NULL:
-            res = db.select('settings_tb', where={'settings_tb.key': id}, what="value")
-            res = list(res)
+        # 查询某一个/所有垃圾站点的数据
+        if id is None:
+            res = db.select('settings_tb', what="value")
+            res = to_list(res)
         else:
             res = db.select('settings_tb', where={'settings_tb.key': id}, what="value")
             res = to_json(res[0])
@@ -54,12 +61,19 @@ class settings_index:
         return res
 
     def POST(self, data):
-        # 修改jetson数据库
+        # 修改数据库中jetson的设置
         id = data.id
-        db.update('settings_tb', where={'settings_tb.key': id}, what="value")
+        res = db.select('settings_tb', what="value")
+        res = to_json(res[0])
+        data.curCapacity = res.curCapacity
+        db.update('settings_tb', where={'settings_tb.key': id}, value=json.dumps(data))
 
-        # 往jetson发送一条数据
+        # 修改jetson中的设置
         requests.post(url=jetson_url, data=data)
+    
+    def PUT(self, data):
+        # 添加jetson的设置
+        db.insert('settings_tb', key=id, value=json.dumps(data))
 
 
 if __name__ == "__main__":
